115ed9d593e4e7a8cff157feb4ebae242b95942a
anggito4rpl5 <anggito_abimanyu_33rpl@student.smktelkom-mlg.sch.id>
2024-08-05 14:07:58 +0700

    Upload Pertama

---
+++ ito.java
@@ -0,0 +1,21 @@
+/**
+ * ito
+ */
+public class ito {
+
+    public static void main(String[] args) {
+        String nama="Anggito";
+        String alamat="malang";
+        String asal="kepanjen";
+        int usia=15;
+        char jeniskelamin='L';
+        double beratbadan=45.00;
+        System.out.println("nama: "+nama);
+        System.out.println("alamat:"+alamat );
+        System.out.println("asal=: "+asal);
+        System.out.println("usia: "+usia);
+        System.out.println("Jenis Kelamin: "+ jeniskelamin);
+        System.out.println("Berat badan: "+beratbadan);
+
+    }
+}
